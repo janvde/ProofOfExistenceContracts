3fd6b98fdf60ed5c935640040f5b313dee8c4a07
Jan van den Ende <jan@getroadmap.com>
2018-12-13 23:10:46 +0100

    Initial commit

---
+++ contracts/ProofOfExistence.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.24;
+
+contract ProofOfExistence {
+    event ProofSet(
+        bytes32 proof,
+        uint blockNumber
+    );
+
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    mapping(bytes32 => uint) public proofs;
+
+    function setProof(bytes32 proof) public {
+        require(proofs[proof] == 0);
+
+        proofs[proof] = block.number;
+
+        emit ProofSet(proof, block.number);
+    }
+
+    function hasProof(bytes32 proof) public view returns (uint) {
+        return proofs[proof];
+    }
+}
